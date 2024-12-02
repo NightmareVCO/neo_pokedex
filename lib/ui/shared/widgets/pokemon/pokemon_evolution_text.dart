700380abf50d7da35c2c05d512e80859f41f7d5b
NightmareVCO <vladimircuriel@outlook.com>
2024-12-02 15:40:11 -0400

    feat(evolutions): improve evolutions render

--- lib/ui/shared/widgets/pokemon/pokemon_evolution_text.dart
+++ lib/ui/shared/widgets/pokemon/pokemon_evolution_text.dart
@@ -72,127 +72,245 @@ class PokemonEvolutionText extends StatelessWidget {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+        child: Center(child: _buildEvolutionLayout(context)),
+      ),
+    );
+  }
+
+  Widget _buildEvolutionLayout(BuildContext context) {
+    final totalEvolutions = pokemonEvolutionTextDto.length;
+
+    if (totalEvolutions == 3 &&
+        _hasNoEvolutionDetails(pokemonEvolutionTextDto[2])) {
+      return _buildThreeEvolutionsSpecialLayout(context);
+    } else if (totalEvolutions <= 3) {
+      return _buildSimpleRow(context);
+    } else if (totalEvolutions == 4) {
+      return _buildFourEvolutionsLayout(context);
+    } else if (totalEvolutions == 5) {
+      return _buildFiveEvolutionsLayout(context);
+    } else {
+      return _buildManyEvolutionsLayout(context);
+    }
+  }
+
+  bool _hasNoEvolutionDetails(PokemonEvolutionTextDto evolution) {
+    return evolution.level == 'Unknown' &&
+        evolution.item == 'Unknown' &&
+        evolution.happiness == 'Unknown' &&
+        evolution.trigger == 'Unknown';
+  }
+
+  Widget _buildThreeEvolutionsSpecialLayout(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _buildEvolutionCard(pokemonEvolutionTextDto[0], context),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Center(child: Icon(Icons.arrow_forward, size: 24)),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildEvolutionCard(pokemonEvolutionTextDto[1], context),
+            const SizedBox(height: 16),
+            _buildEvolutionCard(pokemonEvolutionTextDto[2], context),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildEvolutionCard(
+      PokemonEvolutionTextDto evolution, BuildContext context) {
+    return Container(
+      width: 160,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: pokemonTypeColors[evolution.types.first]!.withOpacity(0.5),
+            blurRadius: 2,
+          ),
+        ],
+      ),
+      child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: _buildEvolutionWidgets(context),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () => {
+                if (originId != int.parse(evolution.id))
+                  {
+                    Navigator.pushNamed(
+                      context,
+                      '/pokemon_page',
+                      arguments: int.parse(evolution.id),
+                    )
+                  }
+              },
+              child: Image.network(
+                evolution.imageUrl,
+                width: 80,
+                height: 80,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Text(
+              evolution.level != 'Unknown'
+                  ? 'Lv. ${evolution.level}'
+                  : evolution.item != 'Unknown'
+                      ? 'Item: ${evolution.item}'
+                      : evolution.happiness != 'Unknown'
+                          ? 'Happiness: ${evolution.happiness}'
+                          : evolution.trigger != 'Unknown'
+                              ? evolution.trigger
+                              : '',
+              style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey),
+            ),
+            Text(
+              evolution.name,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: pokemonTypeColors[evolution.types.first]),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: evolution.types.map((type) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 4.0, vertical: 2),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: pokemonTypeColorsBg[type.toLowerCase()],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          getPokemonTypeIcon(type,
+                              size: 12,
+                              color: const ColorFilter.mode(
+                                  Colors.white, BlendMode.srcIn)),
+                          const SizedBox(width: 2),
+                          Text(
+                            type.toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  List<Widget> _buildEvolutionWidgets(BuildContext context) {
-    List<Widget> widgets = [];
-
-    for (int i = 0; i < pokemonEvolutionTextDto.length; i++) {
-      final evolution = pokemonEvolutionTextDto[i];
-
-      widgets.add(Container(
-        width: 160,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          boxShadow: [
-            BoxShadow(
-              color: pokemonTypeColors[evolution.types.first]!.withOpacity(0.5),
-              blurRadius: 2,
-            ),
+  Widget _buildFourEvolutionsLayout(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _buildEvolutionCard(pokemonEvolutionTextDto[0], context),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Center(child: Icon(Icons.arrow_forward, size: 24)),
+        ),
+        _buildEvolutionCard(pokemonEvolutionTextDto[1], context),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Center(child: Icon(Icons.arrow_forward, size: 24)),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildEvolutionCard(pokemonEvolutionTextDto[2], context),
+            const SizedBox(height: 16),
+            _buildEvolutionCard(pokemonEvolutionTextDto[3], context),
           ],
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              GestureDetector(
-                onTap: () => {
-                  if (originId != int.parse(evolution.id))
-                    {
-                      Navigator.pushNamed(
-                        context,
-                        '/pokemon_page',
-                        arguments: int.parse(evolution.id),
-                      )
-                    }
-                },
-                child: Image.network(
-                  evolution.imageUrl,
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.contain,
-                ),
-              ),
-              Text(
-                evolution.level != 'Unknown'
-                    ? 'Lv. ${evolution.level}'
-                    : evolution.item != 'Unknown'
-                        ? 'Item: ${evolution.item}'
-                        : evolution.happiness != 'Unknown'
-                            ? 'Happiness: ${evolution.happiness}'
-                            : evolution.trigger != 'Unknown'
-                                ? evolution.trigger
-                                : '',
-                style: const TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey),
-              ),
-              Text(
-                evolution.name,
-                style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: pokemonTypeColors[evolution.types.first]),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: evolution.types.map((type) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 4.0, vertical: 2),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: pokemonTypeColorsBg[type.toLowerCase()],
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(3.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            getPokemonTypeIcon(type,
-                                size: 12,
-                                color: const ColorFilter.mode(
-                                    Colors.white, BlendMode.srcIn)),
-                            const SizedBox(width: 2),
-                            Text(
-                              type.toUpperCase(),
-                              style: const TextStyle(
-                                fontSize: 14,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ],
-          ),
-        ),
-      ));
+      ],
+    );
+  }
 
-      if (i < pokemonEvolutionTextDto.length - 1) {
-        widgets.add(const Padding(
+  Widget _buildFiveEvolutionsLayout(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _buildEvolutionCard(pokemonEvolutionTextDto[0], context),
+        const Padding(
           padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Icon(
-            Icons.arrow_forward,
-            size: 24,
-          ),
-        ));
-      }
-    }
+          child: Center(child: Icon(Icons.arrow_forward, size: 24)),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildEvolutionCard(pokemonEvolutionTextDto[1], context),
+            const SizedBox(height: 16),
+            _buildEvolutionCard(pokemonEvolutionTextDto[3], context),
+          ],
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Center(child: Icon(Icons.arrow_forward, size: 24)),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildEvolutionCard(pokemonEvolutionTextDto[2], context),
+            const SizedBox(height: 16),
+            _buildEvolutionCard(pokemonEvolutionTextDto[4], context),
+          ],
+        ),
+      ],
+    );
+  }
 
-    return widgets;
+  Widget _buildManyEvolutionsLayout(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: pokemonEvolutionTextDto
+          .map((evolution) => Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: _buildEvolutionCard(evolution, context),
+              ))
+          .toList(),
+    );
+  }
+
+  Widget _buildSimpleRow(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: List.generate(pokemonEvolutionTextDto.length * 2 - 1, (index) {
+        if (index.isEven) {
+          return _buildEvolutionCard(
+              pokemonEvolutionTextDto[index ~/ 2], context);
+        }
+        return const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Center(child: Icon(Icons.arrow_forward, size: 24)),
+        );
+      }),
+    );
   }
 }
